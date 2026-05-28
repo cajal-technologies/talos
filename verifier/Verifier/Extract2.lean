8eab336507beb76b2e6492afc03cef3d82720754
Marcelo Fornet <mfornet94@gmail.com>
2026-05-29 01:07:14 +0200

    Keep only latest Extract

--- verifier/Verifier/Extract2.lean
+++
@@ -1,6 +0,0 @@
-import Verifier.Extract2.Schema
-import Verifier.Extract2.Git
-import Verifier.Extract2.Source
-import Verifier.Extract2.Rust
-import Verifier.Extract2.Program
-import Verifier.Extract2.Run
