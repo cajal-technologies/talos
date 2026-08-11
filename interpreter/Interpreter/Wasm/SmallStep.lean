ca92d6a55e645659396caeddf6a40b9ddac3983d
Marcelo Fornet <mfornet94@gmail.com>
2026-08-11 10:52:14 +0200

    Update Lean toolchain to 4.33.0

--- interpreter/Interpreter/Wasm/SmallStep.lean
+++ interpreter/Interpreter/Wasm/SmallStep.lean
@@ -6275,7 +6275,10 @@ by
 termination_by firstMemOpDepth config
 decreasing_by
   subst_vars
-  simp [firstMemOpDepth, isMemOp] <;> assumption
+  simp only [firstMemOpDepth] <;> try assumption
+  rw [hi]
+  change 0 < 1
+  omega
 
 set_option maxHeartbeats 5000000 in
 theorem stepChecked?_complete {config config' : Config α} {kind : StepKind} :
@@ -6420,7 +6423,11 @@ by
 termination_by firstMemOpDepth config
 decreasing_by
   subst_vars
-  simp [firstMemOpDepth, isMemOp] <;> assumption
+  simp only [firstMemOpDepth] <;> try assumption
+  change (if isMemOp _ = true then 1 else 0) < 1
+  simp_all only [Bool.false_eq_true]
+  simp only [if_false]
+  omega
 
 theorem step_iff {config config' : Config α} {kind : StepKind} :
     stepChecked? config = .ok (some (kind, config')) ↔ Step config kind config' :=
