172ac81e10554e99f556ef238ebc1d11a3e1be2d
Marcelo Fornet <mfornet94@gmail.com>
2026-08-11 09:51:18 +0200

    interpreter: export measure termination lemmas

--- interpreter/Interpreter/Wasm.lean
+++ interpreter/Interpreter/Wasm.lean
@@ -5,6 +5,7 @@ import Interpreter.Wasm.Continuation
 import Interpreter.Wasm.Semantics
 import Interpreter.Wasm.Semantics.Lemmas
 import Interpreter.Wasm.SmallStep
+import Interpreter.Wasm.MeasureTermination
 import Interpreter.Wasm.Wp.Defs
 import Interpreter.Wasm.Wp.Atomic
 import Interpreter.Wasm.Wp.Block
