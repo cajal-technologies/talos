6d8c297d6da3ab6b7b79aaea014013889c714aa5
Daniel Liao <OMSIDaniel@gmail.com>
2026-09-12 13:47:12 -0700

    programs: prove the representation test of the decode-error subtree

--- programs/lean/Project.lean
+++ programs/lean/Project.lean
@@ -81,3 +81,4 @@ import Project.RustHashMap.Func36Proof
 import Project.RustHashMap.Func32Proof
 import Project.RustHashMap.Func31Proof
 import Project.RustHashMap.Func34Proof
+import Project.RustHashMap.Func33Proof
